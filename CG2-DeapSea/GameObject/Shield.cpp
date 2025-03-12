c4c4ca28159606d47874cd3425a032fda9428078
MasamiTuzukibashi <k022g0080@g.neec.ac.jp>
2025-03-12 11:34:12 +0900

    微調整

--- CG2-DeapSea/GameObject/Shield.cpp
+++ CG2-DeapSea/GameObject/Shield.cpp
@@ -1,50 +1,50 @@
 #include "Shield.h"
 
-	Shield::~Shield()
-	{
-	}
+Shield::~Shield()
+{
+}
 
-	void Shield::Initialize()
+void Shield::Initialize()
+{
+    object3d_ = std::make_unique<Object3d>();
+    object3d_->Initialize();
+    modelManager_ = ModelManager::GetInstance();
+    modelManager_->LoadModel(shieldModel, shieldSkin, true);
+    object3d_->SetModel(shieldModel);
+    Model* model = modelManager_->FindModel(shieldModel);
+    Model::ModelData* modelData = model->GetModelData();
+    for (Model::VertexData& vertex : modelData->vertices)
 	{
-	    object3d_ = std::make_unique<Object3d>();
-	    object3d_->Initialize();
-	    modelManager_ = ModelManager::GetInstance();
-	    modelManager_->LoadModel(shieldModel, shieldSkin, true);
-	    object3d_->SetModel(shieldModel);
-	    Model* model = modelManager_->FindModel(shieldModel);
-	    Model::ModelData* modelData = model->GetModelData();
-	    for (Model::VertexData& vertex : modelData->vertices)
-		{
-		    vertex.normal.x = vertex.position.x;
-		    vertex.normal.y = vertex.position.y;
-		    vertex.normal.z = vertex.position.z;
-	    }
-	    model->Memcpy();
-	    isDead = true;
-	    collision.center = {0.0f, 0.0f, 0.0f};
-	    collision.radius = 0.0f;
+	    vertex.normal.x = vertex.position.x;
+	    vertex.normal.y = vertex.position.y;
+	    vertex.normal.z = vertex.position.z;
     }
+    model->Memcpy();
+    isDead = true;
+    collision.center = {0.0f, 0.0f, 0.0f};
+    collision.radius = 0.0f;
+   }
 
-	void Shield::Update(Vector3 playerPos,Vector3 whalePos) 
+void Shield::Update(Vector3 playerPos,Vector3 whalePos) 
+{
+    object3d_->SetTranslate({(playerPos.x + whalePos.x) / 2, (playerPos.y + whalePos.y) / 2, (playerPos.z + whalePos.z) / 2});
+    if (isDead == false)
 	{
-	    object3d_->SetTranslate({(playerPos.x + whalePos.x) / 2, (playerPos.y + whalePos.y) / 2, (playerPos.z + whalePos.z) / 2});
-	    if (isDead == false)
+	    if (collision.radius <= Length(Subtract(playerPos,object3d_->GetTranslate())) + 2.0f)
 		{
-		    if (collision.radius <= Length(Subtract(playerPos,object3d_->GetTranslate())) + 2.0f)
-			{
-			    collision.radius += 0.2f;
-		    }
-		}
-		else if (collision.radius > 0.0f)
-		{
-		    collision.radius -= 0.2f;
-		}
-	    object3d_->SetScale({collision.radius, collision.radius, collision.radius});
-	    collision.center = object3d_->GetTranslate();
-	    object3d_->Update(Camera::GetInstance());
+		    collision.radius += 0.2f;
+	    }
 	}
+	else if (collision.radius > 0.0f)
+	{
+	    collision.radius -= 0.2f;
+	}
+    object3d_->SetScale({collision.radius, collision.radius, collision.radius});
+    collision.center = object3d_->GetTranslate();
+    object3d_->Update(Camera::GetInstance());
+}
 
-	void Shield::Draw()
-	{
-		object3d_->Draw(modelManager_->GetModelCommon());
-	}
+void Shield::Draw()
+{
+	object3d_->Draw(modelManager_->GetModelCommon());
+}
